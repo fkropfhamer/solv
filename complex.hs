d3803d2ce4aaf32a9058fee0e3e02f494573c868
fkropfhamer <fabian.kropfhamer@gmail.com>
2021-07-31 22:51:04 +0200

    cleanup.

--- complex.hs
+++ complex.hs
@@ -1,18 +1,17 @@
-data ComplexNumber a b = ComplexNumber a b
+data ComplexNumber a b = ComplexNumber  {
+  im :: a,
+  re :: b
+}
 
 showComplexNumber :: Show a => Show b => ComplexNumber a b -> String
 showComplexNumber (ComplexNumber x y) = show x  ++ "+" ++ show y ++ "i"
 
-
 instance (Show a, Show b) => Show (ComplexNumber a b) where
   show x = showComplexNumber x
 
-re :: ComplexNumber a b -> a
-re (ComplexNumber x y) = x
-
-im :: ComplexNumber a b -> b
-im (ComplexNumber x y) = y
-
 add :: (Num a, Num b) => ComplexNumber a b -> ComplexNumber a b -> ComplexNumber a b
 add (ComplexNumber x y) (ComplexNumber s t) = ComplexNumber (x + s) (y + t)
+
+instance (Eq a, Eq b) => Eq (ComplexNumber a b) where
+  (==) (ComplexNumber a b) (ComplexNumber x y) = a == x && b == y
 
