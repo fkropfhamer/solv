31b4f7900ca7dc6e2d33855962b8aacfbc2c1d97
Fabian Kropfhamer <fabian.kropfhamer@gmail.com>
2024-01-09 07:26:16 +0100

    add nix config.

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {}
+}:
+(pkgs.haskellPackages.developPackage {
+  root = pkgs.nix-gitignore.gitignoreSourcePure [
+    "dist-newstyle"
+    ".*#"
+    ".git"
+  ] ./.;
+
+  modifier = drv: pkgs.haskell.lib.overrideCabal drv (attrs: {
+    buildTools = with pkgs.haskellPackages; (attrs.buildTools or []) ++ [
+      cabal-install
+      hakyll
+      pkgs.linkchecker
+    ];
+  });
+})
+
