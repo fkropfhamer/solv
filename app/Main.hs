d8df81bd2823c2d399119f4b8050e771a257eb84
Fabian Kropfhamer <fabian.kropfhamer@gmail.com>
2024-01-10 04:13:54 +0000

    add tests

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,11 @@
 module Main where
 import Parser
 import Parser.Util
+import System.Environment
 
 main :: IO ()
 main = do 
+    args <- getArgs
+    mapM putStrLn args
     Parser.Util.printHello
     putStrLn $ Parser.parse "A"
